194ac1cbc9a683fd366623c6b9b3e73912542d32
Om Prakash Mallik <opm202000@gmail.com>
2024-02-07 16:35:52 +0530

    Learning about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React from 'react'
+import Comp1 from './Comp1'
+import Comp2 from './Comp2'
+import Calculator from './Calculator'
+import Demo from './Demo'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      {/* <Comp1/> */}
+      {/* <Comp2/> */}
+      <Calculator />
+      {/* <Demo/> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
